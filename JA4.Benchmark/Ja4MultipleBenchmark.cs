a212690aca8c8f368439db6fb14d89762e494113
Eduardo Caceres <edcaceres@microsoft.com>
2025-08-28 17:58:26 +0200

    Add version with improved grease

--- JA4.Benchmark/Ja4MultipleBenchmark.cs
+++ JA4.Benchmark/Ja4MultipleBenchmark.cs
@@ -164,4 +164,28 @@ public class Ja4MultipleBenchmark
 
         return result;
     }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public ulong Improved_Grease(int iterations)
+    {
+        ulong result = 0;
+
+        for (int i = 0; i < iterations; ++i)
+        {
+            foreach (var  (expectedFingerPrint, bytes) in TestCases)
+            {
+                var fingerprint = Ja4_Improved_Grease.EncodeJa4Fingerprint(bytes, bytes.Length);
+
+                if (fingerprint != expectedFingerPrint)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                result += (ulong)fingerprint.Length;
+            }
+        }
+
+        return result;
+    }
 }
--- JA4.Benchmark/Ja4SingleBenchmark.cs
+++ JA4.Benchmark/Ja4SingleBenchmark.cs
@@ -146,4 +146,18 @@ public class Ja4SingleBenchmark
 
         return fingerprint;
     }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public string Improved_Grease(string expectedFingerprint, byte[] bytes)
+    {
+        var fingerprint = Ja4_Improved_Grease.EncodeJa4Fingerprint(bytes, bytes.Length);
+
+        if (fingerprint != expectedFingerprint)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return fingerprint;
+    }
 }
